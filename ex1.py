7973f48412ba391e323218713759cc23201d7ddd
ShemBane <shembane@gmail.com>
2018-06-23 19:43:47 +0300

    first few exercises

---
+++ ex1.py
@@ -0,0 +1,7 @@
+print("Hello World")
+print("Hello again")
+print("I like typing this")
+print("this is fun")
+print("I like printing")
+print("I'd rather you 'not'")
+print('I "said" do not touch this') 
---
+++ ex2.py
@@ -0,0 +1,9 @@
+# A comment this is so you can read your program later
+# Anithing after # is ignord by python
+
+print("I could have code like this.") #then the comment after is ignored
+
+# You can have a comment to 'disable' or comment out code:
+# print("This won't run")
+
+print("This will run")
