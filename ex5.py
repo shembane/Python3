5381340772aee85536a2a92ed1685d4726f866d6
ShemBane <shembane@gmail.com>
2018-06-25 17:04:42 +0300

    more python

---
+++ ex5.py
@@ -0,0 +1,17 @@
+name = 'Shem Bane'
+age = '18'
+height = '1.5' #meters
+weight = '35' #kg
+eye = 'green'
+teeth = 'white'
+hair = 'brown' #lie
+
+print (f"lets talk about {name}.")
+print (f"he's {height} meters tall")
+print (f"he's {weight} kg heavy")
+print("Actually thats not so heavy")
+print(f"he's got {eye} eyes and {hair} hair")
+print(f"he's teeth are usually {teeth} depending on the tea")
+
+total = age + height + weight
+print(f"if I add {age}, {height} and {weight} I get {total}")
