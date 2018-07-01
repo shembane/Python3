700cd7b08588d086c73684c56b5d283fb630ef6a
ShemBane <shembane@gmail.com>
2018-07-01 16:42:31 +0300

    more

---
+++ ex6.py
@@ -0,0 +1,21 @@
+type_of_pepoe = 10
+x = f"there are {type_of_pepoe} types of pepole."
+binary = "binary"
+do_not = "don't"
+y = f"those who know {binary} and those who {do_not}"
+
+print (x)
+print (y)
+
+print (f"if I said: {x}")
+print (f"I also said: '{y}'")
+
+hilarious = False
+
+joke_evaluation = "Isn't that joke so funny?! {}"
+print (joke_evaluation.format(hilarious))
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+print (w + e)
